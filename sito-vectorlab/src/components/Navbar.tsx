ebfaa3c41529f1b6a82f30bed7dd29979d55e244
MarcOttoC <108730642+MarcOttoC@users.noreply.github.com>
2022-10-07 17:29:32 +0200

    navbar completed, missing responsive

--- sito-vectorlab/src/components/Navbar.tsx
+++ sito-vectorlab/src/components/Navbar.tsx
@@ -2,16 +2,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-function NavbarSite() {
+interface Props {
+
+  fastNav: (index:number) => void
+}
+
+const NavbarSite:React.FunctionComponent<Props> = (props) =>{
+
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand>VectorLab</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick={() => props.fastNav(0)}>VR</Nav.Link>
+            <Nav.Link onClick={() => props.fastNav(1)}>AR</Nav.Link>
+            <Nav.Link onClick={() => props.fastNav(2)}>GameDev</Nav.Link>
+            <Nav.Link onClick={() => props.fastNav(3)}>Gamification</Nav.Link>
+            <Nav.Link onClick={() => props.fastNav(4)}>3D</Nav.Link>
+            <Nav.Link onClick={() => props.fastNav(5)}>ProceduralAI</Nav.Link>
+            <Nav.Link onClick={() => props.fastNav(6)}>Animation</Nav.Link>
+            <Nav.Link onClick={() => props.fastNav(7)}>Academy</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
