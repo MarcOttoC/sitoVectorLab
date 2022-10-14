15e71d90e96b5ac36293e2281428a9dc65e0629f
FrancescoPiga <francesco.piga.95@gmail.com>
2022-10-14 15:41:56 +0200

    projection complete

--- sito-vectorlab/src/rendererComponents/ArAppRenderer.tsx
+++ sito-vectorlab/src/rendererComponents/ArAppRenderer.tsx
@@ -38,40 +38,44 @@ class ArAppRenderer extends React.Component{
   yMax = Math.PI/4
   yMin = -Math.PI/4
 
+  SMX!: number
+  A!: number
+  X!: number
+  radius = 30
+
+
   componentDidMount(): void {
 
     const FOV = 50
-    const degreePerPixel = FOV / window.innerWidth
-    //const smx = (e.clientX - this.rect.left) - (window.innerWidth/2)
+
 
     this.rect = this.myCanvas.getBoundingClientRect()
-    this.renderer = new WebGLRenderer({ canvas: this.myCanvas, /*alpha:true, */antialias:true});
+    this.renderer = new WebGLRenderer({ canvas: this.myCanvas, alpha:true, antialias:true});
     this.scene = new Scene();
     this.camera = new PerspectiveCamera(FOV);
-    this.camera.position.set(0, 0, 300);
+    this.camera.position.set(0, 0, this.radius);
     this.light = new DirectionalLight(0xffffff, 1.0);
     this.light.position.set(5, 4, 0);
     //var orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
     this.scene.add(this.light,this.helper);
     this.update();
     this.material = new MeshNormalMaterial;
-    this.geometry = new SphereGeometry(4, 30, 30);
+    this.geometry = new SphereGeometry(1, 30, 30);
     this.sphere = new Mesh(this.geometry, this.material);
     this.scene.add(this.sphere,this.helper);
-    this.renderer.setSize(this.myCanvas.width, this.myCanvas.height);    
+    this.renderer.setSize(this.myCanvas.width, this.myCanvas.height);  
+    const degreePerPixel = FOV / this.rect.width
 
     this.myCanvas.addEventListener("mousemove", e => { 
      // this.mouse.y = Math.floor((Math.max(Math.min(-(e.clientY / this.myCanvas.height) * 2 + 1,this.yMax),this.yMin))*100)
       //this.mouse.x = Math.floor((Math.max(Math.min(((e.clientX / this.myCanvas.width) * 2 - 1),this.xMax),this.xMin)*100))
-      this.sphere.position.x = (e.clientX - this.rect.left);  
-     // this.sphere.position.y = -(e.clientY - this.rect.top)/10;
-  })
+      //this.sphere.position.x = ((e.clientX - this.rect.left)-this.rect.width/2)/80;  
+      //this.sphere.position.y = (-(e.clientY - this.rect.top)+this.rect.height/2)/80;
+      this.SMX = (e.clientX - this.rect.left) - (this.rect.width/2)
+      this.A=degreePerPixel * this.SMX
+      this.sphere.position.x = Math.tan(this.A* Math.PI / 180)*this.radius
 
-    this.myCanvas.addEventListener("mousedown", e => {
-
-      console.log(e.clientX - this.rect.left,e.clientY - this.rect.top)
     })
-
   }
 
   componentWillUnmount(): void {
